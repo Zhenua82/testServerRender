36dfcb5440363fd36141bf4462ceb541704941c6
Zhenua82 <zzhmenka@yandex.ru>
2025-10-06 14:32:07 +0300

    06.10.2025

--- server.js
+++ server.js
@@ -52,7 +52,9 @@ const allowedOrigins = [
   'https://testserver-eight-olive.vercel.app',
   'https://testserverrender.onrender.com',
   'http://localhost:5173',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'http://localhost:3000',
+  'http://127.0.0.1:3000'
 ];
 
 app.use(cors({
