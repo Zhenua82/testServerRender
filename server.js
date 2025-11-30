eaf082b0e4357fb17cc5392f94154f4697ca216b
Zhenua82 <zzhmenka@yandex.ru>
2025-11-30 18:00:37 +0300

    addGHPServer

--- server.js
+++ server.js
@@ -54,7 +54,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'https://zhenua82.github.io/expGHPServer/'
 ];
 
 app.use(cors({
